64d98dfd1100ff1ed14d94d7cbd1207f1602198d
michael-re <102764781+michael-re@users.noreply.github.com>
2024-05-18 20:12:36 -0700

    feat: add sudoku grid representation

--- genetic-algorithm/include/ga/sudoku.hpp
+++ genetic-algorithm/include/ga/sudoku.hpp
@@ -3,6 +3,9 @@
 #ifndef GA_SUDOKU_HPP
 #define GA_SUDOKU_HPP
 
+#include <array>
+#include <utility>
+
 #include "ga/puzzle.hpp"
 
 namespace ga
@@ -11,8 +14,31 @@ namespace ga
         : public puzzle
     {
     public:
+        struct detail
+        {
+            static constexpr auto grid_w     = 9;
+            static constexpr auto grid_h     = 9;
+            static constexpr auto sub_grid_w = 3;
+            static constexpr auto sub_grid_h = 3;
+
+            struct cell
+            {
+                bool stochastic = true;
+                char value      = '0';
+            };
+
+            using grid = std::array<cell, grid_w * grid_h>;
+        };
+
+    public:
+        [[nodiscard]] auto cell(const int x, const int y)       ->       detail::cell&;
+        [[nodiscard]] auto cell(const int x, const int y) const -> const detail::cell&;
+
         [[nodiscard]] virtual auto read (std::istream& stream)       -> std::istream& override;
         [[nodiscard]] virtual auto write(std::ostream& stream) const -> std::ostream& override;
+
+    private:
+        detail::grid m_grid = { };
     };
 } // namespace ga
 
--- genetic-algorithm/source/ga/sudoku.cpp
+++ genetic-algorithm/source/ga/sudoku.cpp
@@ -1,5 +1,15 @@
 #include "ga/sudoku.hpp"
 
+auto ga::sudoku::cell(const int x, const int y) -> detail::cell&
+{
+    return m_grid.at((y * detail::grid_w) + x);
+}
+
+auto ga::sudoku::cell(const int x, const int y) const -> const detail::cell&
+{
+    return m_grid.at((y * detail::grid_w) + x);
+}
+
 auto ga::sudoku::read(std::istream &stream) -> std::istream &
 {
     throw std::runtime_error("unimplemented function - ga::sudoku::read");
