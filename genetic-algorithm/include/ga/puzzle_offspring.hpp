8f900fda4f2d344316a8ff66d1f06d52df30c5a5
michael-re <102764781+michael-re@users.noreply.github.com>
2024-05-25 18:06:30 -0700

    feat: create abstract class `puzzle_offspring`

---
+++ genetic-algorithm/include/ga/puzzle_offspring.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef GA_PUZZLE_OFFSPRING_HPP
+#define GA_PUZZLE_OFFSPRING_HPP
+
+#include "ga/puzzle.hpp"
+
+namespace ga
+{
+    class puzzle_offspring
+    {
+    public:
+        explicit puzzle_offspring() = default;
+        virtual ~puzzle_offspring() = default;
+
+        [[nodiscard]] virtual auto clone_puzzle  (const puzzle& puzzle) const -> ga::puzzle* = 0;
+        [[nodiscard]] virtual auto make_offspring(const puzzle& puzzle) const -> ga::puzzle* = 0;
+    };
+} // namespace ga
+
+#endif // GA_PUZZLE_OFFSPRING_HPP
