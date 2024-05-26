a4df2567a587ab4d5e5aafba984790434c9f056b
michael-re <102764781+michael-re@users.noreply.github.com>
2024-05-25 18:09:31 -0700

    feat: create concrete class `sudoku_offspring`

---
+++ genetic-algorithm/include/ga/sudoku_offspring.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef GA_SUDOKU_OFFSPRING_HPP
+#define GA_SUDOKU_OFFSPRING_HPP
+
+#include "ga/puzzle_offspring.hpp"
+#include "ga/sudoku.hpp"
+
+namespace ga
+{
+    class sudoku_offspring final
+        : public puzzle_offspring
+    {
+    public:
+        [[nodiscard]] virtual auto clone_puzzle  (const puzzle& puzzle) const -> ga::puzzle* override;
+        [[nodiscard]] virtual auto make_offspring(const puzzle& puzzle) const -> ga::puzzle* override;
+    };
+} // namespace ga
+
+#endif // GA_SUDOKU_OFFSPRING_HPP
---
+++ genetic-algorithm/source/ga/sudoku_offspring.cpp
@@ -0,0 +1,11 @@
+#include "ga/sudoku_offspring.hpp"
+
+auto ga::sudoku_offspring::clone_puzzle(const puzzle& puzzle) const -> ga::puzzle*
+{
+    throw std::runtime_error("unimplemented function - ga::sudoku_offspring::clone_puzzle");
+}
+
+auto ga::sudoku_offspring::make_offspring(const puzzle& puzzle) const -> ga::puzzle*
+{
+    throw std::runtime_error("unimplemented function - ga::sudoku_offspring::make_offspring");
+}
