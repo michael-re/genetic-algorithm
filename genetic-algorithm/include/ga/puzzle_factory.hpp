22d2b241ade7bfd1fa54067298d3ae7cd65b0906
michael-re <102764781+michael-re@users.noreply.github.com>
2024-05-26 20:58:57 -0700

    feat: create abstract class `puzzle_factory`

---
+++ genetic-algorithm/include/ga/puzzle_factory.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef GA_PUZZLE_FACTORY_HPP
+#define GA_PUZZLE_FACTORY_HPP
+
+#include "ga/puzzle.hpp"
+
+namespace ga
+{
+    class puzzle_factory
+    {
+    public:
+        explicit puzzle_factory() = default;
+        virtual ~puzzle_factory() = default;
+
+        [[nodiscard]] virtual auto create_puzzle()                     const ->     puzzle* = 0;
+        [[nodiscard]] virtual auto create_puzzle(const puzzle& puzzle) const -> ga::puzzle* = 0;
+        [[nodiscard]] virtual auto clone_puzzle (const puzzle& puzzle) const -> ga::puzzle* = 0;
+    };
+} // namespace ga
+
+#endif // GA_PUZZLE_FACTORY_HPP
