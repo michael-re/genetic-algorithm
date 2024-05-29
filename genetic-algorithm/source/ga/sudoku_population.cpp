5517dd8dbd47984cfed7a212885c18f24cb9499e
michael-re <102764781+michael-re@users.noreply.github.com>
2024-05-28 19:26:01 -0700

    feat: implement sudoku population control methods

--- genetic-algorithm/source/ga/sudoku_population.cpp
+++ genetic-algorithm/source/ga/sudoku_population.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "ga/sudoku_population.hpp"
 #include "ga/sudoku.hpp"
 
@@ -10,12 +12,53 @@ ga::sudoku_population::sudoku_population(const class sudoku& source, const std::
 
 auto ga::sudoku_population::cull(const float percent) -> void
 {
-    static_cast<void>(percent);
+    if (percent != 0.0f)
+    {
+        const auto pop_cull_rate = static_cast<std::size_t>(m_size * std::min(1.0f, std::abs(percent)));
+        const auto pop_cull_size = static_cast<std::ptrdiff_t>((m_size - pop_cull_rate) + 1u);
+        const auto least_fit_beg = m_individuals.begin() + pop_cull_size;
+        const auto least_fit_end = m_individuals.end();
+        m_individuals.erase(least_fit_beg, least_fit_end);
+
+        // remove at least one puzzle during cull phase
+        if ((m_individuals.size() - 1) == m_size)
+            m_individuals.pop_back();
+    }
 }
 
 auto ga::sudoku_population::new_generation() -> void
 {
-    
+    if (m_individuals.size() == 1)
+    {
+        // create new generation from progenitor
+        m_individuals.reserve(m_size + 1);
+        while ((m_individuals.size() - 1) != m_size)
+            m_individuals.push_back(m_individuals.front().offspring());
+    }
+    else
+    {
+        // create new generation from most fit puzzles in old generation
+        std::swap(m_individuals.front(), m_individuals.back());
+        auto progenitor = std::move(m_individuals.back());
+        m_individuals.pop_back();
+
+        const auto old_generation = std::move(m_individuals);
+        m_individuals.clear();
+        m_individuals.reserve(m_size + 1);
+        m_individuals.push_back(std::move(progenitor));
+
+        while ((m_individuals.size() - 1) != m_size)
+        {
+            for (const auto& individual : old_generation)
+            {
+                m_individuals.push_back(individual.offspring());
+                if ((m_individuals.size() - 1) == m_size) break;
+            }
+        }
+    }
+
+    // sort puzzles from most fit to least fit
+    std::sort(m_individuals.begin() + 1, m_individuals.end());
 }
 
 auto ga::sudoku_population::best_fitness() const -> int
